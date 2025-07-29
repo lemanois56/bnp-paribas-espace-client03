176b55a3401f8917f5f94e2464e16a3b2a43f279
lemanois56 <lemanolivier7@gmail.com>
2025-07-29 20:59:46 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,13 +31,13 @@ const resetS = () => {
 
 
 /** solde javascript **/
- const alert_block=()=>{
-     Swal.fire(
-         'COMPTE BLOQUÉ',
-         'Votre compte est bloqué. Veuillez contacter votre conseiller',
-         'warning',
-       )
- }
+// const alert_block=()=>{
+//     Swal.fire(
+ //        'COMPTE BLOQUÉ',
+ //        'Votre compte est bloqué. Veuillez contacter votre conseiller',
+ //        'warning',
+  //     )
+// }
 /** End solde javascript **/
 
 
